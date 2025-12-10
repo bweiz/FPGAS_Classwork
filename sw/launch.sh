78b288aa6982ac0cc9ff17948a07958c68121b7a
Mark-H-W <kk7byr@wildmile.com>
2025-12-09 23:41:03 -0700

    more bash nonsense

--- sw/launch.sh
+++ sw/launch.sh
@@ -3,12 +3,12 @@ pb_pid=""
 pot_pid=""
 bar_pid=""
 
-if [ $1 -eq "y" ]; then
+if [ $1 = "y" ]; then
     pb_pid=$(bash ./custom_pb_colors.sh &)
     pot_pid=$(./pot_to_rgb &)
 fi
 
-if [ $2 -eq "y" ]; then
+if [ $2 = "y" ]; then
     bar_pid=$(bash ./update_led_bar.sh &)
 fi
 
@@ -16,11 +16,11 @@ fi
 read -p "Press enter to exit" response
 
 
-if [ $1 -eq "y" ]; then
+if [ $1 = "y" ]; then
     kill "$pb_pid"
     kill "$pot_pid"
 fi
 
-if [ $2 -eq "y" ]; then
+if [ $2 = "y" ]; then
     kill "$bar_pid"
 fi
