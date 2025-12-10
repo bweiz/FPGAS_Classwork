d2f17e09f5166dd2e8cde63de88b77022844ea85
Mark-H-W <kk7byr@wildmile.com>
2025-12-09 23:53:20 -0700

    bash fixes

--- sw/launch.sh
+++ sw/launch.sh
@@ -4,12 +4,15 @@ pot_pid=""
 bar_pid=""
 
 if [ $1 = "y" ]; then
-    pb_pid=$(bash ./custom_pb_colors.sh &)
-    pot_pid=$(./pot_to_rgb &)
+    bash ./custom_pb_colors.sh &
+    pb_pid=$!
+    ./pot_to_rgb &
+    pot_pid=$!
 fi
 
 if [ $2 = "y" ]; then
-    bar_pid=$(bash ./update_led_bar.sh &)
+    bash ./update_led_bar.sh &
+    bar_pid=$!
 fi
 
 
