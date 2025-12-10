02e446ce0dda1d9c130da85f6aeb7ad39ad4971a
Mark-H-W <kk7byr@wildmile.com>
2025-12-09 22:13:24 -0700

    more bash fixes

--- sw/update_led_bar.sh
+++ sw/update_led_bar.sh
@@ -3,5 +3,5 @@ SW_LED_CONTROL="/sys/devices/platform/ff37f450.ledbar/sw_led_control"
 while true
 do
     proc_count=$(ps -e --no-headers | wc -l)
-    echo proc_count > $SW_LED_CONTROL
+    echo $proc_count > $SW_LED_CONTROL
 done
